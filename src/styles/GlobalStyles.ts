6f0cec87d0c60375ae9c324ccf8c670b5428e7bf
luisanegri <luisaanegri@gmail.com>
2020-09-11 20:47:31 +0200

    initial commit

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500&display=swap');
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body, #root {
+        height: 100%
+    } 
+
+    *, button, input {
+        border: 0;
+        outline: 0;
+        font-family: 'Barlow', sans-serif;
+    }
+
+    button {
+        cursor: pointer
+    }
+
+    h1 {
+        font-weight: 500
+    }
+
+`;
